2644039eb90076c56f0d9ba4bc653e399aba296e
pablojr17 <pablojr17@gmail.com>
2023-04-27 00:33:10 -0300

    Concluido

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoItemParaDeletar = new EventEmitter();
 
   faPen = faPen;
-  faTrash = faTrash
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {}
 
-  ngOnInit(): void { }
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
 
+  deletarItem() {
+    this.emitindoItemParaDeletar.emit(this.item.id);
+  }
+
+  checarItem() {
+    if (this.item.comprado == true) {
+      this.item.comprado = false;
+    } else {
+      this.item.comprado = true;
+    }
+  }
+
+  ngOnDestroy() {}
 }
--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -2,36 +2,50 @@ import { Item } from 'src/app/interfaces/iItem';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ListaDeCompraService {
-
-  private listaDeCompra: Item[] = [
-    {
-      "id": 1,
-      "nome": "Queijo prato",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 2,
-      "nome": "Leite integral",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": false
-    },
-    {
-      "id": 3,
-      "nome": "Mamão papaia",
-      "data": "Segunda-feira (31/10/2022) às 08:30",
-      "comprado": true
-    },
-  ]
+  private listaDeCompra: Item[];
 
   constructor() {
+    this.listaDeCompra = JSON.parse(localStorage.getItem('itens') || '[]');
     console.log('Instanciando dependências necessárias para o serviço.');
   }
 
-  getListaDeCompra(){
+  getListaDeCompra() {
     return this.listaDeCompra;
+  }
+
+  criarItem(nomeItem: string) {
+    const id = this.listaDeCompra.length + 1;
+    const item: Item = {
+      id: id,
+      nome: nomeItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: false,
+    };
+
+    return item;
+  }
+
+  adicionarItemNaLista(nomeDoItem: string) {
+    const item = this.criarItem(nomeDoItem);
+    this.listaDeCompra.push(item);
+    // this.atualizarLocalStorage();
+  }
+
+  editarItemDaLista(itemAntigo: Item, nomeEditadoDoItem: string) {
+    const id = itemAntigo.id;
+    const itemEditado: Item = {
+      id: itemAntigo.id,
+      nome: nomeEditadoDoItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: itemAntigo.comprado,
+    };
+    this.listaDeCompra.splice(Number(id) - 1, 1, itemEditado);
+    // this.atualizarLocalStorage();
+  }
+  atualizarLocalStorage() {
+    localStorage.setItem('itens', JSON.stringify(this.listaDeCompra));
   }
 }
